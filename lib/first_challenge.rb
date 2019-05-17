8d02a2c693a56f34f4732235c536b47210148a48
Angela Lo <angelawenlo@gmail.com>
2019-05-17 18:49:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,8 +17,7 @@ def first_challenge
   #your code here
 
   contacts.each do |name, info|
-    binding.pry
-
+    if info.delete_if("strawberry")
   #remember to return your newly altered contacts hash!
   end
 end
