34dac9092e7bc1c28447e903091c33db55aea80a
Angela Lo <angelawenlo@gmail.com>
2019-05-17 19:09:57 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,7 +20,7 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavors|
-          flavors.delete_if "strawberry"
+          flavors.delete "strawberry"
     end
   end
 end
