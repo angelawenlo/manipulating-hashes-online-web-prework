5630da66f8f3d14bbe1f3d1958c86f91871cd119
Angela Lo <angelawenlo@gmail.com>
2019-05-17 19:23:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,5 +1,10 @@
 def second_challenge
-
+  groceries = {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+   }
 
   #code your solution here!
 
